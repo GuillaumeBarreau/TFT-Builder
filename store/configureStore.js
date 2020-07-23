62230e20333823f5e1fb8a9b0d6d99788b5c8d08
GuillaumeBarreau <guillaume.barreau@zenika.com>
2020-07-23 10:55:26 +0200

    🐛fix Reux store

--- store/configureStore.js
+++ store/configureStore.js
@@ -1,6 +1,9 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import rootReducer from './reducers/rootReducer';
 
-const store = createStore(rootReducer);
+const middlewares = [thunk];
+
+const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
 export default store;
